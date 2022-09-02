9bca8209bc7e06209ca79ae9c4e50101ad54cf07
Cameron Tredoux <camtredoux@gmail.com>
2022-09-01 23:30:51 -0600

    Transferred portfolio from vite to next

---
+++ src/components/sections/Navigation.tsx
@@ -0,0 +1,120 @@
+import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
+import { FaChevronUp } from "react-icons/fa";
+
+interface NavigationProps {
+  projInView: boolean;
+  aboutInView: boolean;
+}
+
+export const Navigation: React.FC<NavigationProps> = ({
+  projInView,
+  aboutInView,
+}) => {
+  const [selected, setSelected] = useState(0);
+
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    if (!scrolled) {
+      setSelected(0);
+    }
+    if (projInView || !aboutInView) {
+      setSelected(0);
+    }
+    if (aboutInView) {
+      setSelected(1);
+    }
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [scrolled, projInView, aboutInView]);
+
+  const handleScroll = () => {
+    if (window !== undefined) {
+      const pos = window.scrollY;
+      if (pos > 40) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    }
+  };
+
+  const handleResumeClick = () => {
+    // window.open(Pdf);
+  };
+
+  const links = [
+    {
+      text: "projects",
+      href: "#projects",
+    },
+    {
+      text: "about",
+      href: "#about",
+    },
+    {
+      text: "resume",
+      href: "/resume.pdf",
+    },
+  ];
+
+  return (
+    <div className="relative flex justify-center sm:justify-between items-center mb-10 sm:mb-14">
+      <motion.img
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1, ease: "easeInOut" }}
+        src="/logo.png"
+        className="w-28"
+        alt="Cameron's Logo"
+      />
+      <motion.ul
+        className={`hidden sm:flex gap-10 text-gray-500 text-lg ${
+          scrolled && "fixed top-5 z-50 bg-white drop-shadow-md p-4 rounded-md"
+        }`}
+        initial={{
+          opacity: 0,
+        }}
+        animate={{
+          opacity: 1,
+        }}
+        transition={{ duration: 1, ease: "easeInOut" }}
+      >
+        {/* {`${inView}`} */}
+        {links.map((link, index) => (
+          <a
+            rel={"noreferrer"}
+            onClick={link.text === "resume" ? handleResumeClick : undefined}
+            href={link.href}
+            key={index}
+          >
+            <motion.li className="relative" onClick={() => setSelected(index)}>
+              {link.text}
+              {index === selected && (
+                <motion.div
+                  layoutId="links"
+                  className="w-full h-4 -z-10 bottom-3 left-2 bg-[#ABC4FF55] absolute"
+                ></motion.div>
+              )}
+            </motion.li>
+          </a>
+        ))}
+        {scrolled && (
+          <a
+            className="text-xs p-2 bg-gray-50 hover:bg-gray-100 rounded-sm"
+            href="#"
+          >
+            <motion.li>
+              <FaChevronUp />
+            </motion.li>
+          </a>
+        )}
+      </motion.ul>
+    </div>
+  );
+};
---
+++ src/pages/index.tsx
@@ -0,0 +1,114 @@
+import { motion } from "framer-motion";
+import { NextPage } from "next";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+import { useInView } from "react-intersection-observer";
+import { About } from "../components/sections/About";
+import { Navigation } from "../components/sections/Navigation";
+import { Projects } from "../components/sections/Projects";
+import { Arrow } from "../components/svgs/Arrow";
+import { GithubGrid } from "../components/tools/GithubGrid";
+
+const Home: NextPage = () => {
+  const { ref: projRef, inView: projInView } = useInView({
+    threshold: 0.5,
+  });
+  const { ref: aboutRef, inView: aboutInView } = useInView({
+    threshold: 1,
+  });
+
+  const drawLine = {
+    initial: { opacity: 0.8, pathLength: 0, strokeWidth: 0 },
+    hover: {
+      opacity: 0.8,
+      pathLength: 1,
+      strokeWidth: 3,
+    },
+  };
+
+  const drawText = {
+    initial: { opacity: 0 },
+    animate: {
+      opacity: 1,
+      transition: { delay: 0.5, ease: "easeInOut", duration: 1 },
+    },
+  };
+
+  return (
+    <div className="selection:bg-sky-50 selection:text-blue-400 px-8 my-12 mx-auto max-w-4xl font-inter font-bold">
+      <Navigation projInView={projInView} aboutInView={aboutInView} />
+      <div className="relative">
+        <motion.div
+          whileHover="hover"
+          initial="initial"
+          animate="animate"
+          className="cursor-pointer hidden sm:block text-3xl text-gray-700"
+        >
+          <Arrow variants={drawLine} />
+          <motion.div variants={drawText}>That&apos;s me</motion.div>
+        </motion.div>
+      </div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 1, ease: "easeInOut", duration: 1 }}
+        className="text-base text-gray-500 px-1 mt-2"
+      >
+        I&apos;m a third-year{" "}
+        <span className="text-blue-400">computer science student</span> at CU
+        Boulder and a{" "}
+        <span className="text-blue-400">software engineer intern</span> at
+        Keysight Technology .
+        <div className="mt-8">
+          <div className="flex gap-8 text-xl">
+            <a
+              rel="noreferrer"
+              href="https://github.com/camerontredoux"
+              target={"_blank"}
+              className="hover:text-[#ABC4FF]"
+            >
+              <FaGithub />
+            </a>
+            <a
+              rel="noreferrer"
+              href="https://linkedin.com/in/camerontredoux"
+              target={"_blank"}
+              className="hover:text-[#ABC4FF]"
+            >
+              <FaLinkedinIn />
+            </a>
+          </div>
+        </div>
+        {/* insert github contribution board, and most recent projects list, do something for linked in maybe */}
+        <GithubGrid />
+        <div ref={projRef} id="projects"></div>
+        <Projects />
+        <div ref={aboutRef} id="about" className="mt-24"></div>
+        <About />
+      </motion.div>
+      <div className="flex justify-center mt-20 h-12 font-normal text-sm">
+        <p>
+          Designed and developed by <strong>me</strong> with{" "}
+          <a
+            rel="noreferrer"
+            className="text-purple-400"
+            target="_blank"
+            href="https://framer.com/motion"
+          >
+            framer-motion
+          </a>{" "}
+          and{" "}
+          <a
+            rel="noreferrer"
+            className="text-sky-400"
+            target="_blank"
+            href="https://reactjs.org"
+          >
+            react
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
